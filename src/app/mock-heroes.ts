c486b129bd80edb4300062962d73be8fbf2f18b3
Spazz1101 <joshua.pompa01@gmail.com>
2021-03-25 20:05:41 -0500

    5 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from "./hero";
 
 export const HEROES: Hero[] = [
-  { id: 11, name: "Dr Nice" },
-  { id: 12, name: "Narco" },
-  { id: 13, name: "Bombasto" },
-  { id: 14, name: "Celeritas" },
-  { id: 15, name: "Magneta" },
-  { id: 16, name: "RubberMan" },
-  { id: 17, name: "Dynama" },
-  { id: 18, name: "Dr IQ" },
-  { id: 19, name: "Magma" },
-  { id: 20, name: "Tornado" }
+  { id: 11, name: "Dr Nice", superpower: "Teleportation" },
+  { id: 12, name: "Narco", superpower: "Invisibility" },
+  { id: 13, name: "Bombasto", superpower: "Explosions" },
+  { id: 14, name: "Celeritas", superpower: "None" },
+  { id: 15, name: "Magneta", superpower: "Magnetism" },
+  { id: 16, name: "RubberMan", superpower: "Elasticity" },
+  { id: 17, name: "Dynama", superpower: "Electricity" },
+  { id: 18, name: "Dr IQ", superpower: "Telepathy" },
+  { id: 19, name: "Magma", superpower: "Lava" },
+  { id: 20, name: "Tornado", superpower: "Air Bender" }
 ];
